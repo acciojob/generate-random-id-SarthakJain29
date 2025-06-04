987f085cd4aabc7589eb1f1b583f50fb97d13d39
Sarthak Jain <93116216+SarthakJain29@users.noreply.github.com>
2025-06-04 23:08:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function makeid(l) {
-  // write your code here
+ const char = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+  let res = " ";
+
+	for(let i=0;i<l;i++){
+		const randomindex = Math.floor(Math.random() * char.length);
+		res += char[randomindex];
+	}
+	return res;
 }
 
 // Do not change the code below.
